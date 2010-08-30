4f9ee69e4b382e10864a43a159e6317a7340252b
Justin Tulloss <justin@rd.io>
2010-08-30 02:51:57 -0700

    add another socket

--- tests/simple-client.js
+++ tests/simple-client.js
@@ -3,6 +3,8 @@ sys = require('sys');
 
 ctx = new zmq.Context();
 s = new zmq.Socket(ctx, zmq.Socket.ZMQ_REQ);
+s1 = new zmq.Socket(ctx, zmq.Socket.ZMQ_REQ);
+
 msg = process.argv[2];
 
 s.on('receive', function(data) {
@@ -11,3 +13,11 @@ s.on('receive', function(data) {
 });
 s.connect('tcp://127.0.0.1:5554');
 s.send(msg);
+
+s1.on('receive', function(data) {
+    sys.puts('other socket received: ' + data);
+    s1.close();
+});
+
+s1.connect('tcp://127.0.0.1:5554');
+s1.send('I\'ve got a lovely bunch of coconuts');
