488a92aeeb8e26c9f41dbbc1ca77d348746c3d5d
Justin Tulloss <justin.tulloss@gmail.com>
2011-07-16 16:22:29 -0700

    added examples provided by jfd to fix his bug

---
+++ examples/sub.js
@@ -0,0 +1,18 @@
+var NO = 383;
+
+var zmq = require("zeromq");
+
+
+var b = new Buffer(4);
+b[0] = NO >>> 24;
+b[1] = NO >>> 16;
+b[2] = NO >>> 8;
+b[3] = NO % 256;
+
+var socket = zmq.createSocket("sub");
+socket.connect("tcp://127.0.0.1:9000");
+
+socket.subscribe(b.toString("binary"));
+socket.on("message", function(msg) {
+  console.log(msg);
+});
