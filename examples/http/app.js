96ae0e42a48902dce723b9a1c161498745b67e4c
TJ Holowaychuk <tj@vision-media.ca>
2012-03-13 20:21:27 -0700

    misc

--- examples/http/app.js
+++ examples/http/app.js
@@ -5,12 +5,16 @@ var zmq = require('../../')
   , Stream = require('stream')
 
 var server = http.createServer(function(req, res){
-  res.writeHead(200, req.headers);
-  req.on('data', function(chunk){
-    res.write(chunk);
-  }).on('end', function(){
-    res.end();
-  });
+  // res.writeHead(200, req.headers);
+  // req.on('data', function(chunk){
+  //   res.write(chunk);
+  // }).on('end', function(){
+  //   res.end();
+  // });
+
+  var body = 'Hello';
+  res.writeHead(200, { 'Content-Length': body.length });
+  res.end(body);
 });
 
 var socks = {};
