31e0b50fc2cfa5eaf643ee4f61919b2e6fa08d30
TJ Holowaychuk <tj@vision-media.ca>
2012-03-13 19:55:43 -0700

    map

--- examples/http/app.js
+++ examples/http/app.js
@@ -1,24 +1,47 @@
 
 var zmq = require('../../')
-  , sock = zmq.socket('dealer');
+  , sock = zmq.socket('dealer')
+  , http = require('http')
+  , Stream = require('stream')
+
+var server = http.createServer(function(req, res){
+  console.log(req.url);
+  console.log(req.method);
+});
+
+var socks = {};
 
 sock.on('message', function(envelope, id, type, data){
   var id = id.toString()
     , type = type.toString()
-    , req;
+    , req
+    , res;
 
   switch (type) {
     case 'request':
-      req = JSON.parse(data.toString());
+      req = new Stream;
+      res = new Stream;
+      res.socket = req;
+      socks[id] = req;
+      data = JSON.parse(data.toString());
+      req.url = data.url;
+      req.method = data.method;
+      req.headers = data.header;
       console.log('%s : %s "%s"', id, req.method, req.url);
+      server.emit('request', req, res);
       break;
     case 'data':
       console.log();
       console.log('data');
       console.log(data.toString());
+      req = socks[id];
+      req.emit('data', data);
       break;
     case 'end':
       console.log('end');
+      req = socks[id];
+      req.emit('end');
+      delete socks[id];
       break;
   }
 });
