bd94669d9f76477b203b06516ce6fe68eb15b5a4
TJ Holowaychuk <tj@vision-media.ca>
2012-03-13 19:38:48 -0700

    misc

--- examples/http/app.js
+++ examples/http/app.js
@@ -2,19 +2,22 @@
 var zmq = require('../../')
   , sock = zmq.socket('dealer');
 
-sock.on('message', function(envelope, id, type, json){
+sock.on('message', function(envelope, id, type, data){
   var id = id.toString()
     , type = type.toString()
     , req;
 
   switch (type) {
     case 'request':
-      req = JSON.parse(json.toString());
+      req = JSON.parse(data.toString());
       console.log('%s : %s "%s"', id, req.method, req.url);
       break;
     case 'data':
+      console.log('data');
+      console.log(data.toString());
       break;
     case 'end':
+      console.log('end');
       break;
   }
 });
