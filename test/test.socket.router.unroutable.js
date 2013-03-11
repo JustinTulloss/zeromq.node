9babd4107901542825935a7e5a31624b2837f324
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2013-03-11 13:32:57 +0700

    Fixed semver usage in the test

--- test/test.socket.router.unroutable.js
+++ test/test.socket.router.unroutable.js
@@ -3,7 +3,7 @@ var zmq = require('../')
   , should = require('should')
   , semver = require('semver');
 
-if (!semver.gte('3.2')) {
+if (!semver.gte(zmq.version, '3.2')) {
   console.warn('Test requires libzmq >= 3.2');
   return;
 }
