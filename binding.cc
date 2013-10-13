59796ce3a9226a6c5e2313f1f587aae1b9042904
Johny Jose <johny@playlyfe.com>
2013-10-14 02:36:54 +0530

    fixed zmq version check for xpub/xsub sockets.
    Signed-off-by: Johny Jose <johny@playlyfe.com>

--- binding.cc
+++ binding.cc
@@ -988,7 +988,7 @@ namespace zmq {
     NODE_DEFINE_CONSTANT(target, ZMQ_CAN_DISCONNECT);
     NODE_DEFINE_CONSTANT(target, ZMQ_PUB);
     NODE_DEFINE_CONSTANT(target, ZMQ_SUB);
-    #if ZMQ_VERSION_MAJOR == 3
+    #if ZMQ_VERSION_MAJOR >= 3
     NODE_DEFINE_CONSTANT(target, ZMQ_XPUB);
     NODE_DEFINE_CONSTANT(target, ZMQ_XSUB);
     #endif
