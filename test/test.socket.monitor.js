643cdc9bf35915db694269a2a730781198480286
Sean Mathews <coder@f34r.com>
2013-11-21 17:16:47 -0800

    added test function

---
+++ test/test.socket.monitor.js
@@ -0,0 +1,50 @@
+
+var zmq = require('../')
+  , should = require('should');
+
+var rep = zmq.socket('rep')
+  , req = zmq.socket('req');
+
+var events = [];
+
+rep.on('message', function(msg){
+  msg.should.be.an.instanceof(Buffer);
+  msg.toString().should.equal('hello');
+  rep.send('world');
+});
+
+rep.on('listen', function(addr,iarg){
+  console.log("listen %s,%d",addr,iarg);
+  addr.toString().should.equal('tcp://127.0.0.1:5555');
+  events.push('listen');
+});
+
+rep.on('accept', function(addr,iarg){
+  console.log("accept %s,%d",addr,iarg);
+  addr.toString().should.equal('tcp://127.0.0.1:5555');
+  events.push('accept');
+});
+
+rep.on('disconnect', function(addr,iarg){
+  console.log("disconnect %s,%d",addr, iarg);
+  addr.toString().should.equal('tcp://127.0.0.1:5555');
+  events.push('disconnect');
+  events.length.should.equal(3);
+});
+
+/* enable monitoring for this socket */
+rep.monitor();
+
+rep.bind('tcp://127.0.0.1:5555');
+
+rep.on('bind', function(){
+  req.connect('tcp://127.0.0.1:5555');
+  req.send('hello');
+  req.on('message', function(msg){
+    msg.should.be.an.instanceof(Buffer);
+    msg.toString().should.equal('world');
+    req.close();
+    rep.close();
+  });
+});
+
