5249ca938337e4584337f92561bd1d2f6baee160
Bent Cardan <bent@nothingsatisfies.com>
2015-01-06 08:06:06 -0500

    reorder semver logic

--- test/socket.stream.js
+++ test/socket.stream.js
@@ -5,61 +5,69 @@ var zmq = require('..')
 
 describe('socket.stream', function(){
 
-  //since its for libzmq4+, we target versions > 4.0.0
-  var version = semver.lte(zmq.version, '4.0.0');
-
   it('should support a streaming socket', function (done){
     
-    if (!version) {
-      done();
-      return console.warn('stream socket type in libzmq v4+');
-    }
+    //socket stream type API after libzmq4+, target > 4.0.0
+    if (semver.gte(zmq.version, '4.0.0')) {
 
-    var stream = zmq.socket('stream');
-    stream.on('message', function (id,msg){
-      
-      msg.should.be.an.instanceof(Buffer);
-      
-      var raw_header = String(msg).split('\r\n');
-      var method = raw_header[0].split(' ')[0];
-      method.should.equal('GET');
-      
-      //due to HTTP GET method, prepare HTTP response for TCP socket
-      var httpProtocolString = 'HTTP/1.0 200 OK\r\n' //status code
-        + 'Content-Type: text/html\r\n' //headers
-        + '\r\n'
-        + '<!DOCTYPE html>' //response body
-          + '<head>'        //make it xml, json, html or something else
-            + '<meta charset="UTF-8">'
-          + '</head>'
-          + '<body>'
-            +'<p>derpin over protocols</p>'
-          + '</body>'
-        +'</html>' 
-
-      //zmq streaming prefixed by envelope's routing identifier
-      stream.send([id,httpProtocolString]);
-    });
-
-    var addr = '127.0.0.1:47080';
-    stream.bind('tcp://'+addr, function(){
-      //send non-peer request to zmq, like an http GET method with URI path
-      http.get('http://'+addr+'/aRandomRequestPath', function (httpMsg){
+      var stream = zmq.socket('stream');
+      stream.on('message', function (id,msg){
         
-        //it's a readable stream as the good lord intended
-        httpMsg.socket._readableState.reading.should.be.true
+        msg.should.be.an.instanceof(Buffer);
         
-        //conventional node streams emit data events to process zmq stream response
-        httpMsg.on('data',function (msg){
-          msg.should.be.an.instanceof(Buffer);
-          String(msg).should.equal('<!DOCTYPE html><head><meta charset="UTF-8"></head>'
-            +'<body>'
+        var raw_header = String(msg).split('\r\n');
+        var method = raw_header[0].split(' ')[0];
+        method.should.equal('GET');
+        
+        //finding an HTTP GET method, prepare HTTP response for TCP socket
+        var httpProtocolString = 'HTTP/1.0 200 OK\r\n' //status code
+          + 'Content-Type: text/html\r\n' //headers
+          + '\r\n'
+          + '<!DOCTYPE html>' //response body
+            + '<head>'        //make it xml, json, html or something else
+              + '<meta charset="UTF-8">'
+            + '</head>'
+            + '<body>'
               +'<p>derpin over protocols</p>'
-            +'</body>'
-          +'</html>');
-          done();
+            + '</body>'
+          +'</html>' 
+
+        //zmq streaming prefixed by envelope's routing identifier
+        stream.send([id,httpProtocolString]);
+      });
+
+      var addr = '127.0.0.1:5513';
+      stream.bind('tcp://'+addr, function(){
+        //send non-peer request to zmq, like an http GET method with URI path
+        http.get('http://'+addr+'/aRandomRequestPath', function (httpMsg){
+          
+          //msg should now be a node readable stream as the good lord intended
+          if (semver.gte(process.versions.node, '0.11.0')){
+            httpMsg.socket._readableState.reading.should.be.false
+          } else {
+            if(semver.gte(process.versions.node, '0.10.0')){
+              httpMsg.socket._readableState.reading.should.be.true
+            }
+          }
+          
+          //conventional node streams emit data events to process zmq stream response
+          httpMsg.on('data',function (msg){
+            msg.should.be.an.instanceof(Buffer);
+            String(msg).should.equal('<!DOCTYPE html><head><meta charset="UTF-8"></head>'
+              +'<body>'
+                +'<p>derpin over protocols</p>'
+              +'</body>'
+            +'</html>');
+            done();
+          });
         });
       });
-    });
+
+    } else {
+
+      done();
+      return console.warn('stream socket type in libzmq v4+');
+    
+    }
   });
 });
