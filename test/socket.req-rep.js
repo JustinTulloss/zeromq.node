24089507d10228c0c8c7cbfd556c75df41abbc1d
Ron Korving <rkorving@wizcorp.jp>
2016-06-02 18:13:36 +0900

    This fixes burst sends on "req" sockets.

--- test/socket.req-rep.js
+++ test/socket.req-rep.js
@@ -56,4 +56,40 @@ describe('socket.req-rep', function(){
     }
   });
 
+  it('should support a burst', function (done) {
+    var rep = zmq.socket('rep');
+    var req = zmq.socket('req');
+    var n = 10;
+
+    rep.on('message', function (msg) {
+      msg.should.be.an.instanceof(Buffer);
+      msg.toString().should.equal('hello');
+      rep.send('world');
+    });
+
+    rep.bind('inproc://reqrepburst', function (error) {
+      if (error) throw error;
+      req.connect('inproc://reqrepburst');
+
+      var received = 0;
+
+      req.on('message', function(msg){
+        msg.should.be.an.instanceof(Buffer);
+        msg.toString().should.equal('world');
+
+        received += 1;
+
+        if (received === n) {
+          rep.close();
+          req.close();
+          done();
+        }
+      });
+
+      for (var i = 0; i < n; i += 1) {
+        req.send('hello');
+      }
+    });
+  });
+
 });
